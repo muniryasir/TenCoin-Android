accc147c302794d6db5e918ea280ff46a7060f94
muniryasir <muniryasir2009@gmail.com>
2024-05-12 04:34:07 +0500

    Add project files.

---
+++ TenCoin/TenCoin.cs
@@ -0,0 +1,12 @@
+using System.Text;
+using System;
+namespace Core
+{
+    public static class TenCoin
+    {
+        public static string getCoinData(string raw)
+        {
+            return "hello";
+        }
+    }
+}
